6042a1b34f7a3375501bc865e2f1ba5a5c23ba03
Math Scicl <scicluna.mathieu@hotmail.fr>
2025-01-23 20:59:26 +0100

    commit

--- backend/config/plugins.ts
+++ backend/config/plugins.ts
@@ -2,7 +2,7 @@ module.exports = ({ env }) => ({
   "users-permissions": {
     config: {
       jwt: {
-        expiresIn: "365d", // Garde l'utilisateur connecté pendant un an
+        expiresIn: "365d", // Garder l'utilisateur connecté pendant 1 an
       },
     },
   },
