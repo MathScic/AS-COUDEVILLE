ad461a9ca7cf6805c28e800094391252a51d97fe
Math Scicl <scicluna.mathieu@hotmail.fr>
2025-01-24 12:54:28 +0100

    commit

--- backend/config/database.ts
+++ backend/config/database.ts
@@ -1,27 +1,18 @@
 import path from "path";
 
 export default ({ env }) => {
-  const client = env("DATABASE_CLIENT", "postgres"); // On passe ici à "postgres"
+  const client = env("DATABASE_CLIENT", "postgres"); // Utiliser "postgres"
 
   const connections = {
     postgres: {
       connection: {
-        connectionString: env("DATABASE_URL"), // Utilisation de la variable d'environnement DATABASE_URL
-        host: env(
-          "DATABASE_HOST",
-          "dpg-cu9mv5lsvqrc73dhpp3g-a.frankfurt-postgres.render.com"
-        ), // Hôte de Render
-        port: env.int("DATABASE_PORT", 5432), // Port PostgreSQL
-        database: env("DATABASE_NAME", "as_db_2ubx"), // Nom de la base de données sur Render
-        user: env("DATABASE_USERNAME", "as_db_2ubx_user"), // Utilisateur PostgreSQL sur Render
-        password: env("DATABASE_PASSWORD", "ovXKSw12lO6RQ1RNbNmgCeYLgp5kH1ok"), // Mot de passe PostgreSQL
+        connectionString: env("DATABASE_URL"), // Utiliser la variable d'environnement DATABASE_URL
         ssl: env.bool("DATABASE_SSL", true) && {
           rejectUnauthorized: env.bool(
             "DATABASE_SSL_REJECT_UNAUTHORIZED",
             false
           ),
         },
-        schema: env("DATABASE_SCHEMA", "public"),
       },
       pool: {
         min: env.int("DATABASE_POOL_MIN", 2),
