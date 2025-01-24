23e2a5033f0db43cd16902fad444564997195d55
Math Scicl <scicluna.mathieu@hotmail.fr>
2025-01-24 11:55:14 +0100

    add postgre plutot que sqlite

--- backend/config/database.ts
+++ backend/config/database.ts
@@ -1,17 +1,20 @@
 import path from "path";
 
 export default ({ env }) => {
-  const client = env("DATABASE_CLIENT", "sqlite");
+  const client = env("DATABASE_CLIENT", "postgres"); // On passe ici à "postgres"
 
   const connections = {
     postgres: {
       connection: {
-        connectionString: env("DATABASE_URL"),
-        host: env("DATABASE_HOST", "dpg-cu96uvhopnds73apohk0-a"),
-        port: env.int("DATABASE_PORT", 5432),
-        database: env("DATABASE_NAME", "as_db"),
-        user: env("DATABASE_USERNAME", "as_db_user"),
-        password: env("DATABASE_PASSWORD", "SuFneUDlkz8LQ5KjeA2Ej0KE4uB68ih8"),
+        connectionString: env("DATABASE_URL"), // Utilisation de la variable d'environnement DATABASE_URL
+        host: env(
+          "DATABASE_HOST",
+          "dpg-cu9mv5lsvqrc73dhpp3g-a.frankfurt-postgres.render.com"
+        ), // Hôte de Render
+        port: env.int("DATABASE_PORT", 5432), // Port PostgreSQL
+        database: env("DATABASE_NAME", "as_db_2ubx"), // Nom de la base de données sur Render
+        user: env("DATABASE_USERNAME", "as_db_2ubx_user"), // Utilisateur PostgreSQL sur Render
+        password: env("DATABASE_PASSWORD", "ovXKSw12lO6RQ1RNbNmgCeYLgp5kH1ok"), // Mot de passe PostgreSQL
         ssl: env.bool("DATABASE_SSL", true) && {
           rejectUnauthorized: env.bool(
             "DATABASE_SSL_REJECT_UNAUTHORIZED",
