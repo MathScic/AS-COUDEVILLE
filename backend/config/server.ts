bb63a8cdfe7bb537eeaa3fdfd31e988cbd6fd9cb
Math Scicl <scicluna.mathieu@hotmail.fr>
2025-01-24 13:34:38 +0100

    commit

--- backend/config/server.ts
+++ backend/config/server.ts
@@ -1,7 +1,9 @@
 export default ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
+  url: env("RENDER_EXTERNAL_URL"), // Ajout de l'URL publique
+  proxy: true, // Activation du proxy
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array("APP_KEYS"),
   },
 });
